8dfe03402c60decca6a718ed180b84e8dbf1d431
Oussama Chatti <oussama.chatti@esprit.tn>
2023-07-17 12:57:14 +0200

    finished building the chatUI--integrating sendbird

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,14 +2,21 @@ import React from "react"
 import Back from "../common/Back"
 import "../home/recent/recent.css"
 import img from "../images/services.jpg"
+import { App as SendbirdApp } from 'sendbird-uikit'
+import "sendbird-uikit/dist/index.css";
+
 
 const Chat = () => {
+  
+  const APP_ID = "9D33B3C8-0189-4B72-B1AB-A71DA20C6AF2";
+  const USER_ID = "oussama.chatti";
+
   return (
     <>
       <section className='blog-out mb'>
         <Back name='Chat' title='Your Group Chats' cover={img} />
-        <div className='container recent'>
-          
+        <div style={{height: "100vh",color:"black"}} className='container recent'>
+          <SendbirdApp appId={APP_ID} userId={USER_ID} />
         </div>
       </section>
     </>
